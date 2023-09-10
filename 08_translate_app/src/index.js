22a91b69ed72b27cab2d096693819929e409b0f1
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-09-10 15:34:10 +0200

    Update index.js

--- 08_translate_app/src/index.js
+++ 08_translate_app/src/index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='wrapper'>
-      <App />
+      <App /> 
     </div>
   </React.StrictMode>
 );
