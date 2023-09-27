ca700940fbb15f9e33e79ec464f684a6cf743241
Artur Sniegowski <49175232+artursniegowski@users.noreply.github.com>
2023-09-27 09:26:37 +0200

    Update App.js

--- 18_media_fetch_app/src/App.js
+++ 18_media_fetch_app/src/App.js
@@ -3,7 +3,7 @@ import UsersList from "./components/UsersList";
 function App() {
     return (
         <div className="container mx-auto">
-            <UsersList />
+            <UsersList /> 
         </div>
     );
 };
